77a9361b53d807f72cbf39d55b3dfce0e66a5485
SashaLah <sasha7Gmail.com>
2024-11-16 19:52:21 -0800

    Lock Node.js and webpack versions, simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,15 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    clean: true,
-    publicPath: '/'
+    filename: 'bundle.js',
+    clean: true
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: 'babel-loader'
       },
       {
         test: /\.css$/,
@@ -30,24 +27,7 @@ module.exports = {
       template: './src/index.html'
     })
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
   resolve: {
     extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-    proxy: {
-      '/api': 'http://localhost:3001'
-    }
   }
 };
